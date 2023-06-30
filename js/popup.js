5f82e13930151ef917a608cffb374ac8c2f20a5b
jsay <jsxhxx0819@gmail.com>
2023-06-30 17:44:41 +0900

    .

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+  var popupBtnLinks = document.querySelectorAll(".popup-btn a");
+  var popupBtn = document.querySelector(".popup-btn");
+  var popupText = document.querySelector(".popup-text");
+  popupBtnLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      popupBtnLinks.forEach(function (item) {
+        item.classList.toggle("active");
+      });
+    });
+  });
+
+  popupBtn.addEventListener("click", function () {
+    popupText.style.display =
+      popupText.style.display === "none" ? "block" : "none";
+  });
+};
